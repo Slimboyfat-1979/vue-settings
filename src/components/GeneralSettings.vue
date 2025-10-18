0e7c838080ed7ac917212ec87f37cc44e93936f3
Slimboyfat-1979 <slimboyfat1979@gmail.com>
2025-10-18 20:38:06 +1300

    added styles to general

--- src/components/GeneralSettings.vue
+++ src/components/GeneralSettings.vue
@@ -1,3 +1,44 @@
 <template>
-    <div>General</div>
+    <div>
+    <h2 class="text-2xl mb-4">General</h2>
+    <form action="" class="space-y-4 mx-auto">
+        <div>
+            <label for="">Username</label>
+            <input type="text">
+        </div>
+        <div>
+            <label for="">Email</label>
+            <input type="email">
+        </div>
+        <div>
+            <label for="">About Me</label>
+            <textarea name="" id=""></textarea>
+        </div>
+        <div>
+            <label for="">Gender</label>
+            <label for="">
+                <input type="radio" value="male">
+                <span>Male</span>
+            </label>
+            <label for="">
+                <input type="radio" value="female">
+                <span>Female</span>
+            </label>
+            <label for="">
+                <input type="radio" value="other">
+                <span>Other</span>
+            </label>
+        </div>
+        <div>
+            <label for="">Country</label>
+            <select name="" id="">
+                <option>USA</option>
+                <option>Canada</option>
+                <option>UK</option>
+            </select>
+        </div>
+        <button type="submit" class="btn-primary">Save</button>
+    </form>
+    </div>
 </template>
+
